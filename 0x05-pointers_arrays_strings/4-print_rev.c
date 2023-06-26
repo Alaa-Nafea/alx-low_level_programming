1827a028036739799113ab1f9f4a2f0ae15f5728
Alaa-Nafea <leenlyan3@gmail.com>
2023-06-26 13:31:43 -0700

    neww

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,15 @@ void print_rev(char *s)
 	while (*s != '\0')
 	{
 		longi++;
-		s++;
+		s++;	
 	}
 	s--;
 	for (o = longi; o > 0; o--)
-}
+	{
 		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+	}
+
